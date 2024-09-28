9423ac1bb7e5355cdf2722086b7fd6e0eef48d6a
dylan <dylanwilkins2308@gmail.com>
2024-09-28 00:46:10 -0400

    added notes

--- PiCalculator.dart
+++ PiCalculator.dart
@@ -39,3 +39,27 @@ class Point {
 
   bool get isInsideUnitCircle => x * x + y * y <= 1;
 }
+
+/* NOTES
+Main Function:
+  computePi() calls the function to start generating points for Pi
+  .take(100) only take 100 values from the stream of estimates
+  await for is similar to a loop but instead waits for a specific action before
+  executing the code within the loop.
+Stream Function:
+  Stream<double> returns a stream of double values 
+  generateRandom().take(batch) takes a batch (collection?) of random points
+  where(p) => p.isInsideUnitCircle chcecks if the points fall within the circle
+GenerateRandom function:
+  random.nextDouble() produces a random value between 0 & 1, it then creates point
+  objects with these random values
+Point class:
+  this defines a point object with two properties (x & y) which represent coords
+  isInsideUnitCircle() checks if the point is inside a circle 
+
+LOGIC FLOW:
+  generates random points
+  checks how many fall inside a circle
+  estimates pi based on the ratio of the points inside the circle to the total points 
+  repeats this proess, refining the estimat as more points are considered
+*/
